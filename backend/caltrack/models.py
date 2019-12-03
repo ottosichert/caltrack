36bb5aff00261e78bd48c93006d4ce76fec095d3
Otto Sichert <git@ottosichert.de>
2019-12-03 16:13:17 +0100

    Make user fields not nullable squash migrations

--- backend/caltrack/models.py
+++ backend/caltrack/models.py
@@ -7,9 +7,9 @@ db = current_app.extensions['db']
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), index=True, unique=True)
-    password_hash = db.Column(db.String(255))
-    daily_calories = db.Column(db.Integer, server_default="2000")
+    username = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    daily_calories = db.Column(db.Integer, server_default="2000", nullable=False)
 
     def __repr__(self):
         return f'User(id={self.id}, username="{self.username}")'
