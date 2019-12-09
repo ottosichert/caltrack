e6af56c5a3104120648c565e258c11db936e359b
Otto Sichert <git@ottosichert.de>
2019-12-09 15:58:40 +0100

    Add tests for meals CRUD operations as admin role

--- frontend/cypress/integration/meals.spec.js
+++ frontend/cypress/integration/meals.spec.js
@@ -1,37 +1,82 @@
-describe('User meals', () => {
+describe('Meals', () => {
   before(() => {
     cy.resetDb();
   });
 
-  beforeEach(() => {
-    cy.login('user', 'user');
+  context('as user', () => {
+    beforeEach(() => {
+      cy.login('user', 'user');
+    });
+
+    it('are shown on dashboard', () => {
+      cy.visit('/portal');
+      cy.get('.table').contains('Porridge');
+    });
+
+    it('can be created', () => {
+      cy.visit('/portal');
+      cy.get('input[name=label]').type('Beer').tab();
+      cy.focused().type('200{enter}');
+      cy.get('.table').contains('Beer').parent().contains('200');
+    });
+
+    it('can be edited', () => {
+      cy.visit('/portal');
+      cy.get('.table').contains('Pizza').parent().contains('Edit').click();
+      cy.get('input[name=label]').clear().type('Half pizza').tab();
+      cy.focused().type('400{enter}');
+      cy.get('.table').contains('Half pizza').parent().contains('400');
+    });
+
+    it('can be deleted', () => {
+      cy.visit('/portal');
+      cy.get('.table').as('table').contains('Pasta').parent().contains('Delete').click();
+      // window.confirm() is automatically accepted by Cypress
+      cy.get('@table').contains('Pasta').should('not.exist');
+    });
   });
 
-  it('are shown on dashboard', () => {
-    cy.visit('/portal');
-    cy.get('.table').as('table').contains('Porridge');
-    cy.get('@table').contains('Nachos');
-  });
+  context('as admin', () => {
+    beforeEach(() => {
+      cy.login('admin', 'admin');
+    });
 
-  it('can be created', () => {
-    cy.visit('/portal');
-    cy.get('input[name=label]').type('Beer').tab();
-    cy.focused().type('200{enter}');
-    cy.get('.table').contains('Beer').parent().contains('200');
-  });
+    it('are shown on dashboard for admin and other users', () => {
+      cy.visit('/portal');
+      cy.get('.table').as('table').contains('Cappucino');
 
-  it('can be edited', () => {
-    cy.visit('/portal');
-    cy.get('.table').contains('Pizza').parent().contains('Edit').click();
-    cy.get('input[name=label]').clear().type('Half pizza').tab();
-    cy.focused().type('400{enter}');
-    cy.get('.table').contains('Half pizza').parent().contains('400');
-  });
+      cy.get('.filter input[name=user_id]').should('be.enabled').type('1{enter}');
+      cy.get('@table').contains('Steak');
+    });
 
-  it('can be deleted', () => {
-    cy.visit('/portal');
-    cy.get('.table').as('table').contains('Pasta').parent().contains('Delete').click();
-    // window.confirm() is automatically accepted by Cypress
-    cy.get('@table').contains('Pasta').should('not.exist');
+    it('can be created for other users', () => {
+      cy.visit('/portal');
+      cy.get('input[name=label]').type('Business breakfast').tab();
+      cy.focused().type('900').tab();
+      cy.focused().type('2{enter}');
+
+      cy.get('.filter input[name=user_id]').should('be.enabled').type('2{enter}');
+      cy.get('.table').contains('Business breakfast');
+    });
+
+    it('can be edited for other users', () => {
+      cy.visit('/portal');
+      cy.get('.filter input[name=user_id]').should('be.enabled').type('1{enter}');
+
+      cy.get('.table').contains('Bacon and eggs').parent().contains('Edit').click();
+      cy.get('input[name=label]').clear().type('Vegan bacon and falafel').tab();
+      cy.focused().type('600').tab();
+      cy.focused().type('1{enter}');
+      cy.get('.table').contains('Vegan bacon and falafel').parent().contains('600');
+    });
+
+    it('can be deleted for other users', () => {
+      cy.visit('/portal');
+      cy.get('.filter input[name=user_id]').should('be.enabled').type('1{enter}');
+
+      cy.get('.table').as('table').contains('Nachos').parent().contains('Delete').click();
+      // window.confirm() is automatically accepted by Cypress
+      cy.get('@table').contains('Nachos').should('not.exist');
+    });
   });
 });
